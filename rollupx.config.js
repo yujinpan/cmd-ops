ccbc6ff6ba9bb16ffb6e4f5911efd7258fc35447
yujinpan <1192878390@qq.com>
2024-04-11 17:57:00 +0800

    chore: support cjs and esm

--- rollupx.config.js
+++ rollupx.config.js
@@ -11,6 +11,21 @@ module.exports = {
 
   outputDir: 'lib',
 
+  formats: [
+    {
+      format: 'es',
+      inputFiles: ['**/*'],
+      outputDir: 'lib/es',
+      outputFile: '[name][ext]',
+    },
+    {
+      format: 'cjs',
+      inputFiles: ['**/*'],
+      outputDir: 'lib/cjs',
+      outputFile: '[name][ext]',
+    },
+  ],
+
   typesOutputDir: 'types',
 
   node: true,
